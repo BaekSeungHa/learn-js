ec0ece5c61f0cab4d96de46ff0b924a4dbfaf44a
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-13 09:37:30 +0900

    논리연산자

--- 220712/4.js
+++ 220712/4.js
@@ -1,3 +1,15 @@
+//삼항 연산자
 var num = 0;
 var result = num === 0 ? "zero" : "not zero";
-console.log(result);
+console.log(result); 
+
+//단축 평가
+if(true || console.log("hello")) {
+
+}
+
+//단축 평가가가 수행됩니다.
+//&&연산자에서 앞의 피연산자가 거짓이면 뒤의 표현식은
+//평가되지 않습니다.
+//||연산자에서 앞의 피연산자가 참이면 뒤의 표현식은
+//평가 되지 않는다
