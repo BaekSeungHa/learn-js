b131ac8412eb46ae3afd144ef2d6e863d1a95244
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-14 10:36:15 +0900

    Math

---
+++ 220714/1.js
@@ -0,0 +1,31 @@
+//내장 객체: 자바스크립트가 기본으로 제공하는 객체
+
+
+//1. Math: 수학과 관련된 기능을 제공하는 객체
+
+//ceil(천장):전달된 값 이상에서 가장 작은 점수를 반환
+// console.log(Math.ceil(1));
+// console.log(Math.ceil(1.000001)); //2
+// console.log(Math.ceil(1.5));
+
+// console.log(Math.ceil(-1));
+// console.log(Math.ceil(-1).);
+
+//floor바닥 전달된 값 이하에서 가장 큰 정수를 반환
+// console.log(Math.floor(1));//1
+// console.log(Math.floor(1.000001));//1
+// console.log(Math.floor(1.5)); //1
+
+//round: 반올림을 수행
+console.log(Math.round(1)); //1
+console.log(Math.round(1.00001)); //1
+console.log(Math.round(1.5)); //2
+
+console.log(Math.round(-1.5)); //-1
+console.log(Math.round(-1.6)); //-2
+
+// 0~10 사이의 정수 난수를 얻고 싶은 경우
+
+
+//Math는 굉장히 많은 기능을 제공합니다. 이를 확인하려면
+//document(도움말)를 확인하면 됩니다.
